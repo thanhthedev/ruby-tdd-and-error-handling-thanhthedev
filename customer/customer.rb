76902c1f4f0934a6ca07130d88cf42189b31a471
caadmin <janel.brandon@coderacademy.edu.au>
2018-09-02 11:41:53 +1000

    fixed up customer.rb

--- customer/customer.rb
+++ customer/customer.rb
@@ -1,9 +1,12 @@
 class Customer
 
-    attr_accessor :first_name
+    attr_accessor :first_name,:last_name
     def initialize(first,last)
         @first_name=first
         @last_name=last
     end
 
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
