c5659ea13e3902159033cd5e46e02f33c8d0df96
caadmin <janel.brandon@coderacademy.edu.au>
2018-09-02 11:04:55 +1000

    Added to_i? method to divide.rb

---
+++ divide.rb
@@ -0,0 +1,33 @@
+# This is a simple divider.
+# Imagine the user tries to divide by zero
+# Ruby will rightfully freak out and crash with a ZeroDivisionError
+# Let's bring in some error handling to display a nice message.
+#
+
+class String
+    def is_i?
+       /\A[-+]?\d+\z/ === self
+    end
+end
+
+def divide (dividend,divisor)
+    quotient = dividend/divisor
+end
+
+puts "--------------------"
+puts "--- Division App ---"
+puts "--------------------"
+begin
+    puts "Give me a number"
+    number1 = gets.chomp.to_i
+    puts "Give me another number"
+    number2 = gets.chomp.to_i
+    answer = divide(number1,number2)
+    print "#{number1} divided by #{number2} = "
+    print "#{answer || "error"}"
+    puts
+rescue
+    puts "Error occured"
+end
+#
+# Code along challenge: if they hit an error, take them back to "give me a number"
