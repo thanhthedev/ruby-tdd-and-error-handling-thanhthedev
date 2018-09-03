0f060c5c6093c4ed08ded027326af3e389c5fad0
caadmin <janel.brandon@coderacademy.edu.au>
2018-09-03 13:29:04 +1000

    fix issues

--- customer/test/customer_test.rb
+++ customer/test/customer_test.rb
@@ -4,16 +4,16 @@ require_relative '../customer'
 class CustomerTest < Test::Unit::TestCase
     def test_customer_first_name
         customer = Customer.new('John','Smith')
-        assert_equal(customer.first_name, 'John')
+        assert_equal('John',customer.first_name)
     end
 
     def test_customer_last_name
         customer = Customer.new('John','Smith')
-        assert_equal(customer.last_name,'Smith')
+        assert_equal('Smith',customer.last_name)
     end
 
     def test_customer_full_name
         customer = Customer.new('John','Smith')
-        assert_equal(customer.full_name,'John Smith')
+        assert_equal('John Smith',customer.full_name)
     end
 end
