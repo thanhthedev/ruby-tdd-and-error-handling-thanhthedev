ec2a42b8edc521a1c0b1900b3b48f3bd69ffd0f5
thanhthedev <huythanhdao@gmail.com>
2018-09-03 15:52:28 +1000

    v1

--- customer/test/customer_test.rb
+++ customer/test/customer_test.rb
@@ -3,17 +3,24 @@ require_relative '../customer'
 
 class CustomerTest < Test::Unit::TestCase
     def test_customer_first_name
-        customer = Customer.new('John','Smith')
-        assert_equal('John',customer.first_name)
+        customer = Customer.new('Jerry','Smith')
+        customer1 = Customer.new("Jamie", "Strong")
+        assert_equal(customer.first_name, 'Jerry')
+        assert_equal(customer1.first_name, 'Jamie')
     end
 
     def test_customer_last_name
-        customer = Customer.new('John','Smith')
-        assert_equal('Smith',customer.last_name)
+        customer = Customer.new('Jerry','Smith')
+        customer1 = Customer.new("Jamie", "Strong")
+        assert_equal(customer.last_name, 'Smith')
+        assert_equal(customer1.last_name, "Strong")
     end
 
     def test_customer_full_name
-        customer = Customer.new('John','Smith')
-        assert_equal('John Smith',customer.full_name)
+        customer = Customer.new('Jerry', 'Smith')
+        customer1 = Customer.new("Jamie", "Strong")
+        assert_equal(customer.full_name, 'Jerry Smith')
+        assert_equal(customer1.full_name, "Jamie Strong")
     end
+
 end
